16b2914ad7d826b65009794fcd503637c405761a
slashispy <andresplanas91@gmail.com>
2018-02-24 16:50:31 -0300

    se agrego el crossOrigin para que el fronEnd tenga permiso para acceder a los webService

--- src/main/java/py/edu/ucsa/rest/api/web/controllers/DepartamentoController.java
+++ src/main/java/py/edu/ucsa/rest/api/web/controllers/DepartamentoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import py.edu.ucsa.rest.api.core.model.Departamento;
 import py.edu.ucsa.rest.api.core.services.DepartamentoService;
 import py.edu.ucsa.rest.api.web.dto.ErrorDTO;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/departamento")
 public class DepartamentoController {
--- src/main/java/py/edu/ucsa/rest/api/web/controllers/TiposExpedController.java
+++ src/main/java/py/edu/ucsa/rest/api/web/controllers/TiposExpedController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import py.edu.ucsa.rest.api.core.model.TiposExped;
 import py.edu.ucsa.rest.api.core.services.TiposExpendService;
 import py.edu.ucsa.rest.api.web.dto.ErrorDTO;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/tipoExped")
 public class TiposExpedController {
